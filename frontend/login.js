65f033c973053e1a8b9c7e0dbfe47efef9462845
aman aswal <amanaswal414@gmail.com>
2026-07-24 10:44:03 +0530

    Use Render backend URL

--- frontend/login.js
+++ frontend/login.js
@@ -9,7 +9,8 @@ loginForm.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/users/login", {
+        const response = await fetch("https://my-freshmart.onrender.com/api/users/login", {
+            
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
